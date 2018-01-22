ace88f34418f76279cbc8e64319fdd9af0b20c45
lo16 <lo16@users.noreply.github.com>
2018-01-22 12:52:53 -0800

    add stemming for input sentence

--- p1test.py
+++ p1test.py
@@ -1,38 +1,42 @@
 import sys
 import re
+from nltk.stem.porter import *
 
 line_num = 1
+
+#patterns used for tokenization
 letternumber = r'[A-Za-z0-9]'
 notletter = r'[^A-Za-z0-9]'
 alwayssep = r'[:|;=!?&]'
 word = r'[A-Za-z0-9]+-?[A-Za-z0-9]*'
 
+#compile regex patterns
 string_obj = re.compile('^\w+-?\w*$')
 int_obj = re.compile('^-?\d+$')
 double_obj = re.compile('^-?\d+\.\d+$')
 op_obj = re.compile('^[:\|;\.=,!\?]$')
 
+#stemmer for input sentence
+stemmer = PorterStemmer()
+
 def tokenize(line):
-	line = re.sub('(' + alwayssep + ')', r' \g<1> ', line)
+    #add whitespace around symbols
+    line = re.sub('(' + alwayssep + ')', r' \g<1> ', line)
 
-	#commas not in numbers
-	line = re.sub('([^0-9]),', r'\g<1> , ', line)
-	line = re.sub(',([^0-9])', r' , \g<1>', line)
+    #add whitespace around commas not in numbers
+    line = re.sub('([^0-9]),', r'\g<1> , ', line)
+    line = re.sub(',([^0-9])', r' , \g<1>', line)
 
-	#single quotes
-	line = re.sub('^\'', '^\' ', line)
-	line = re.sub('(' + notletter + ')\'', r'\g<1> \'', line)
+    #add whitespace around single quotes that are not apostrophes
+    line = re.sub('^\'', '^\' ', line)
+    line = re.sub('(' + notletter + ')\'', r'\g<1> \'', line)
 
-	#periods
-	line = re.sub('(' + letternumber + ')\.$', r'\g<1> .', line)
-	line = re.sub('(' + letternumber + ')\. ', r'\g<1> . ', line)
-	words = line.split()
-	return words
+    #add whitespace around periods that are not decimal points
+    line = re.sub('(' + letternumber + ')\.$', r'\g<1> .', line)
+    line = re.sub('(' + letternumber + ')\. ', r'\g<1> . ', line)
+    words = line.split()
+    return words
 
-# string_obj = re.compile('^\w+$')
-# int_obj = re.compile('^-?\d+$')
-# double_obj = re.compile('^-?\d+\.\d+$')
-# op_obj = re.compile('^[:|;]$')
 def find_type(w):
     if int_obj.match(w):
         return 'INT'
@@ -47,9 +51,20 @@ for line in sys.stdin:
     line = line.strip()
     split_line = tokenize(line)
     if not (split_line == [] or split_line[0] == '#'):
-        for w in split_line:
-    	   print '{0} {1} {2}'.format(w, find_type(w), line_num)#, stem(w))
-    	line_num += 1
+        if split_line[0] == 'W':
+            for w in split_line[:2]:
+                print '{0} {1} {2}'.format(w, find_type(w), line_num)
+
+            #input sentence is here
+            for w in split_line[2:]:
+                if find_type(w) == 'STRING':
+                    print '{0} {1} {2} {3}'.format(w, find_type(w), line_num, stemmer.stem(w))
+                else:
+                    print '{0} {1} {2}'.format(w, find_type(w), line_num)
+        else:
+            for w in split_line:
+                print '{0} {1} {2}'.format(w, find_type(w), line_num)
+        line_num += 1
 print 'ENDFILE\n'
 
 
